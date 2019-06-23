7fa6c93d2045aef4115c76b095876ff6aea3fac4
Alexei Barabash <alexeib@codeoasis.com>
2019-06-23 17:26:29 +0300

    fix

--- vars/slackSendHelper.groovy
+++ vars/slackSendHelper.groovy
@@ -3,7 +3,7 @@ def call(success) {
     wrap([$class: 'BuildUser']) {
         BUILDER_NAME = (BUILD_USER == '' || BUILD_USER == null || BUILD_USER == 'SCMTrigger') ? gitlabUserName : BUILD_USER
     }
-    message = " Job '${JOB_NAME} *[${BRANCH_TO_CLONE}]'* By *${BUILDER_NAME} - _${currentBuild.durationString}_* - ${BUILD_URL}";
+    message = " Job '${JOB_NAME} *[${env.BRANCH_TO_CLONE}]'* By *${BUILDER_NAME} - _${currentBuild.durationString}_* - ${BUILD_URL}";
     message = success ? "*SUCCESSFUL:*" + message :  "*FAILED:*" + message
     color =  success ? '#00FF00' : '#FF0000'
     slackSend(token: SLACK_TOKEN, channel: SLACK_CH, color: color, message: message)
