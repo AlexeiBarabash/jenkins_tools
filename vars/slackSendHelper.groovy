1730e2192968ce3d48ef4d2b6ea9764179863948
Bar Nuri <barn@codeoasis.com>
2019-06-23 17:49:23 +0300

    Update slackSendHelper.groovy

--- vars/slackSendHelper.groovy
+++ vars/slackSendHelper.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 def call(success) {
-    message = " Job '${JOB_NAME} *[${env.BRANCH_TO_CLONE}]'* By *${BUILDER_NAME} - _${currentBuild.durationString}_* - ${BUILD_URL}";
+    message = " Job '${JOB_NAME} *[${env.BRANCH_TO_CLONE}]'* By *${env.BUILDER_NAME} - _${currentBuild.durationString}_* - ${BUILD_URL}";
     message = success ? "*SUCCESSFUL:*" + message :  "*FAILED:*" + message
     color =  success ? '#00FF00' : '#FF0000'
     slackSend(token: SLACK_TOKEN, channel: SLACK_CH, color: color, message: message)
