d074fbfd9f3d1da452545cb5f992c444c6273cb1
Bar Nuri <barn@codeoasis.com>
2019-06-23 17:24:46 +0300

    Update init.groovy

--- vars/init.groovy
+++ vars/init.groovy
@@ -7,7 +7,7 @@ def call() {
     
     // SERVERS = []
     DOCKER_REPO = config.DOCKER_REPO
-
+    BRANCH_TO_CLONE = params.TAG_OR_BRANCH ?: GIT_BRANCH ?: "integration"
     // if(ENV == 'integration') {
     //     SERVERS = SERVERS_INTEGRATION
     //     CONTAINER_RUN_ARGS += " ${CONTAINER_RUN_ARGS_INTEGRATION} "
@@ -28,7 +28,7 @@ def call() {
     //     DOCKER_REPO = DOCKER_REPO_PROD
     // }
             
-    env.BRANCH_TO_CLONE = params.TAG_OR_BRANCH ?: GIT_BRANCH ?: "integration"
+    
 
     CONTAINER_NAME = "${CONTAINER_NAME}-${ENV}"
     CONTAINER_VERSION = "${(params.TAG_OR_BRANCH == null) ? BUILD_NUMBER : BRANCH_TO_CLONE}"
@@ -37,7 +37,8 @@ def call() {
     env.CONTAINER_NAME = CONTAINER_NAME
     env.CONTAINER_VERSION = CONTAINER_VERSION
     env.CONTAINER_NAME_REPO = CONTAINER_NAME_REPO
-    env.DOCKER_REPO = config.DOCKER_REPO
+    env.DOCKER_REPO = DOCKER_REPO
     env.CONTAINER_RUN_ARGS = config.CONTAINER_RUN_ARGS
     env.SERVERS = config.SERVERS
+    env.BRANCH_TO_CLONE = BRANCH_TO_CLONE
 }
