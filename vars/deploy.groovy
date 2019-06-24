8ac1f31f24ae89c60196a6d0421a18f9c92bff5a
Bar Nuri <barn@codeoasis.com>
2019-06-24 08:19:28 +0300

    fix deploy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -3,7 +3,7 @@ def call() {
     if(env.K8S_APPLY_FILES_GLOB != "" && env.K8S_APPLY_FILES_GLOB != null && (fileExists(env.K8S_APPLY_FILES_GLOB))) {
             deploy_k8s()
     } else {
-        for(server in SERVERS) {
+        for(server in env.SERVERS) {
             sshDeploy(server)
         }
     }
