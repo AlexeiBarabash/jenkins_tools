93490dc4fec9c5bb22a1fa4853146230a53b81f4
Bar Nuri <barn@codeoasis.com>
2019-07-03 09:15:47 +0300

    Update deploy_k8s.groovy

--- vars/deploy_k8s.groovy
+++ vars/deploy_k8s.groovy
@@ -1,10 +1,19 @@
 #!/usr/bin/env groovy
 def call() {
     textWithColor("Deploy Container With K8s")
-    kubernetesDeploy(
-        kubeconfigId: env.KUBE_CONFIG_ID,
-        configs: env.K8S_APPLY_FILES_GLOB,
-        enableConfigSubstitution: true
-    )
+    if(env.KUBE_CONFIG_ID == "kubeconfig_codeoasis") {
+        kubernetesDeploy(
+            kubeConfig: "/root/.kube/config.yaml",
+            configs: env.K8S_APPLY_FILES_GLOB,
+            enableConfigSubstitution: true
+        )
+    } else {
+        kubernetesDeploy(
+            kubeconfigId: env.KUBE_CONFIG_ID,
+            configs: env.K8S_APPLY_FILES_GLOB,
+            enableConfigSubstitution: true
+        )
+    }
+
     textWithColor("Finished Deploy Container")
 }
