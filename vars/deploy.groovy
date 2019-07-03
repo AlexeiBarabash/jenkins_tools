4f81eb13f28be3dcea03cf5dcfc62f0730b46057
Bar Nuri <barn@codeoasis.com>
2019-07-03 08:58:34 +0300

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,6 +1,6 @@
 def call() {
     textWithColor("Deploying ${ENV}")
-    if(env.K8S_APPLY_FILES_GLOB != "" && env.K8S_APPLY_FILES_GLOB != null && (fileExists(env.K8S_APPLY_FILES_GLOB))) {
+    if(env.K8S_APPLY_FILES_GLOB != "" && env.K8S_APPLY_FILES_GLOB != null) {
             deploy_k8s()
     } else {
         for(server in (readJSON(text: env.SERVERS))) {
