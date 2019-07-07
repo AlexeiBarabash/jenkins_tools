a2ebe0c7a8c421c960fcbfc9d9e6daad53ecd893
Bar Nuri <barn@codeoasis.com>
2019-07-07 12:23:51 +0300

    Update buildContainer.groovy

--- vars/buildContainer.groovy
+++ vars/buildContainer.groovy
@@ -1,11 +1,11 @@
 #!/usr/bin/env groovy
-def call(args) {
+def call(args, dockerFile = './Dockerfile') {
     textWithColor("Building Container")
     DOCKER_BUILD_ARGS = ''
     args.each { arg,value ->
         DOCKER_BUILD_ARGS = "${DOCKER_BUILD_ARGS} --build-arg ${arg}=${value}"
     }
-    sh "docker build ${DOCKER_BUILD_ARGS} -t ${CONTAINER_NAME} ."
+    sh "docker build ${DOCKER_BUILD_ARGS} -t ${CONTAINER_NAME} -f ${dockerFile} ."
     sh "docker tag ${CONTAINER_NAME} ${CONTAINER_NAME_REPO}"
     sh "docker push ${CONTAINER_NAME_REPO}"
     env.DOCKER_BUILD_ARGS = DOCKER_BUILD_ARGS
