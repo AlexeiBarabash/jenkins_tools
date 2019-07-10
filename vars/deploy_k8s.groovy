b04750e5d06b30f7a28daa5b27851b51771b193f
Bar Nuri <barn@codeoasis.com>
2019-07-10 10:24:23 +0300

    k8s

--- vars/deploy_k8s.groovy
+++ vars/deploy_k8s.groovy
@@ -1,26 +1,10 @@
 #!/usr/bin/env groovy
 def call() {
     textWithColor("Deploy Container With K8s")
-    // if(env.KUBE_CONFIG_ID == "kubeconfig_codeoasis") {
-    // sh """
-    // echo '
-    //     export KUBCONFIG="/var/lib/jenkins/k8sConfig/config.yaml"
-    //     ls  /var/lib/jenkins/k8sConfig/config.yaml
-    //     kubectl --kubeconfig="/var/lib/jenkins/k8sConfig/config.yaml" config view
-    //     kubectl --kubeconfig="/var/lib/jenkins/k8sConfig/config.yaml" get nodes
-    //     envsubst < ${env.K8S_APPLY_FILES_GLOB} | kubectl --kubeconfig="/var/lib/jenkins/k8sConfig/config.yaml" apply -f -
-    //     ' > ./script.sh
-    // """
-    // sh "chmod 777 ./script.sh"
-    // sh "./script.sh"
-
-    // } else {
-        kubernetesDeploy(
-            kubeconfigId: env.KUBE_CONFIG_ID,
-            configs: env.K8S_APPLY_FILES_GLOB,
-            enableConfigSubstitution: true
-        )
-    // }
-
+    kubernetesDeploy(
+        kubeconfigId: env.KUBE_CONFIG_ID,
+        configs: env.K8S_APPLY_FILES_GLOB,
+        enableConfigSubstitution: true
+    )
     textWithColor("Finished Deploy Container")
 }
