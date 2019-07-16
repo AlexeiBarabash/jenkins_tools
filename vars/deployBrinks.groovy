47d7067d9b70a34d9e87045df350ef056eb68a76
Bar Nuri <barn@codeoasis.com>
2019-07-16 13:33:53 +0300

    deployBrinks

---
+++ vars/deployBrinks.groovy
@@ -0,0 +1,25 @@
+def call(sshUser = 'deploy') {
+    for(sshServer in (readJSON(text: env.SERVERS))) {
+        textWithColor("Deploy Container To - ${sshUser}@${sshServer}")
+        def scriptFile = "./script.sh"
+        sh """
+        echo '
+        git reset --hard
+        git fetch --all
+        git checkout ${env.BRANCH_TO_CLONE}
+        git pull
+        git reset --hard
+        git rm -r --cached .
+
+        docker build -t ${env.CONTAINER_NAME} .
+        docker stop ${env.CONTAINER_NAME} || true
+        docker rm ${env.CONTAINER_NAME} || true
+        echo 'docker run command'
+        docker run ${env.CONTAINER_RUN_ARGS} --name ${env.CONTAINER_NAME} ${env.CONTAINER_NAME} || exit 1
+        docker ps -a
+            ' > ${scriptFile}
+        """
+        sh "ssh ${sshUser}@${sshServer} bash -s < ${scriptFile}"
+        textWithColor("Finished Deploy Container")
+    }
+}
