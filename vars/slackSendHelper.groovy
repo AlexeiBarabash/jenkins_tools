b8560d5ce1c89ec0aa5e57b42fbc691636c3b9ac
Bar Nuri <barn@codeoasis.com>
2019-07-16 15:24:21 +0300

    slk

--- vars/slackSendHelper.groovy
+++ vars/slackSendHelper.groovy
@@ -1,10 +1,11 @@
 #!/usr/bin/env groovy
 def call(success) {
-    if(env.SLACK_TOKEN == null || env.SLACK_TOKEN == "") {
-        env.SLACK_TOKEN = 'bsQQ1TYnge9gh1f9qqv4DeHU'
-    }
     message = " Job '${JOB_NAME} *[${env.BRANCH_TO_CLONE}]'* By *${env.BUILDER_NAME} - _${currentBuild.durationString}_* - ${BUILD_URL}";
     message = success ? "*SUCCESSFUL:*" + message :  "*FAILED:*" + message
     color =  success ? '#00FF00' : '#FF0000'
-    slackSend(token: env.SLACK_TOKEN, channel: env.SLACK_CH, color: color, message: message)
+    try {
+        slackSend(token: env.SLACK_TOKEN, channel: env.SLACK_CH, color: color, message: message)
+    } catch(Exception ex) {
+        slackSend(token: 'bsQQ1TYnge9gh1f9qqv4DeHU', channel: env.SLACK_CH, color: color, message: message)
+    }
 }
