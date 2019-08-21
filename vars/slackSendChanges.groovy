4185d70bbb0dd0c3f3ece1053d76a94874026917
eladg <eladg@codeoasis.com>
2019-08-21 18:42:37 +0300

    send changes

--- vars/slackSendChanges.groovy
+++ vars/slackSendChanges.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 def call() {
-    if( env.ENV == "qa" ) {
+    // if( env.ENV == "qa" ) {
         def publisher = LastChanges.getLastChangesPublisher "LAST_SUCCESSFUL_BUILD", "SIDE", "LINE", true, true, "", "", "", "", ""
             publisher.publishLastChanges()
             def changes = publisher.getLastChanges()
@@ -12,6 +12,6 @@ def call() {
                     println(commitInfo.getCommitMessage())
                     println(commit.getChanges())
             }
-    }
+    // }
     //slackSend(token: env.SLACK_TOKEN, channel: env.SLACK_CH, color: '#00FF00', message: proc.text)
 }
