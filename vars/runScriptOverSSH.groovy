6aea850655fde067867d355303bacdf4c9676cb9
Bar Nuri <barn@codeoasis.com>
2019-09-18 09:49:45 +0300

    script

--- vars/runScriptOverSSH.groovy
+++ vars/runScriptOverSSH.groovy
@@ -1,13 +1,12 @@
 #!/usr/bin/env groovy
 def call(script,sshServer, sshUser = 'deploy', pass = '', isWindows = false) {
     textWithColor("Deploy Container To - ${sshUser}@${sshServer}")
-    def scriptFile = "./script.sh"
     sh """
         echo '
         ${script}
-        ' > ${scriptFile}
+        ' > ./script.sh
     """
-    def cmd = "ssh ${sshUser}@${sshServer} ${isWindows ? 'cmd' : 'bash'} -s < ${scriptFile} -o StrictHostKeyChecking=no"
+    def cmd = "ssh ${sshUser}@${sshServer} ${isWindows ? 'cmd' : 'bash'} -s < ./script.sh -o StrictHostKeyChecking=no"
     if(pass != '') {
         cmd = "sshpass -p ${pass} " + cmd
     }
