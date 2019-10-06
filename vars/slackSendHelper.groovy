621e4442ce71e3abda31bd5fe8d7bbc9b845258e
Bar Nuri <barn@codeoasis.com>
2019-10-06 09:28:29 +0300

    slackSendHelper

--- vars/slackSendHelper.groovy
+++ vars/slackSendHelper.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
-def call(success) {
+def call(success, useHttpRequest = false) {
     try {
         if(isEmpty(env.SLACK_TOKEN)) {
             env.SLACK_TOKEN = 'bsQQ1TYnge9gh1f9qqv4DeHU'
@@ -7,6 +7,15 @@ def call(success) {
         message = " Job '${JOB_NAME} *[${env.BRANCH_TO_CLONE}]'* By *${env.BUILDER_NAME} - _${currentBuild.durationString}_* - ${BUILD_URL}";
         message = success ? "*SUCCESSFUL:*" + message :  "*FAILED:*" + message
         color =  success ? '#00FF00' : '#FF0000'
+        if(useHttpRequest) {
+            response = httpRequest (consoleLogResponseBody: true,
+            contentType: 'APPLICATION_JSON',
+            httpMode: 'POST',
+            requestBody: "{ \"text\" : \"" + message +  "\" }",
+            url: "https://codeoasis.slack.com/services/hooks/jenkins-ci/?token=" + env.SLACK_TOKEN,
+            validResponseCodes: '200')
+            return
+        }
         slackSend(token: env.SLACK_TOKEN, channel: env.SLACK_CH, color: color, message: message)
     } catch(Exception ex) {}
 }
