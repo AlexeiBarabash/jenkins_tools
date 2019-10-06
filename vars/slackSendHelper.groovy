2d14493b2d5cee624c9d4b8e4d170de60c4b35c0
Bar Nuri <barn@codeoasis.com>
2019-10-06 12:56:40 +0300

    slackSendHelper

--- vars/slackSendHelper.groovy
+++ vars/slackSendHelper.groovy
@@ -4,6 +4,16 @@ def call(success, useHttpRequest = true) {
         if(isEmpty(env.SLACK_TOKEN)) {
             env.SLACK_TOKEN = 'bsQQ1TYnge9gh1f9qqv4DeHU'
         }
+        if(isEmpty(env.BRANCH_TO_CLONE)) {
+            env.BRANCH_TO_CLONE = 'unknown'
+        }
+        if(isEmpty(env.BUILDER_NAME)) {
+            env.BUILDER_NAME = 'unknown'
+        }
+        if(isEmpty(env.SLACK_CH)) {
+            textWithColor("env.SLACK_CH is missing")
+            return
+        }
         def title = " Job '${JOB_NAME} *[${env.BRANCH_TO_CLONE}]'* By *${env.BUILDER_NAME}*"
         def title_link = BUILD_URL;
         def message = success ? "*SUCCESSFUL*"  :  "*FAILED*"
