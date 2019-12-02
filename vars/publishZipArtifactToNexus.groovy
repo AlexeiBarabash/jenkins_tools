0ff9a826e6e218e9171e48352f7f0833f1a060d1
Bar Nuri <barn@codeoasis.com>
2019-12-02 20:51:31 +0200

    fix publish nesux

--- vars/publishZipArtifactToNexus.groovy
+++ vars/publishZipArtifactToNexus.groovy
@@ -5,7 +5,7 @@ def call(filePath, repo = 'http://registry.codeoasis.com:8081/repository/Files/'
     def fileName = JOB_NAME + '_' + BUILD_NUMBER + '.zip'
     def auth = 'Basic ' + user.bytes.encodeBase64().toString()
     httpRequest (consoleLogResponseBody: true,
-            uploadFile: filePath
+            uploadFile: filePath,
             httpMode: 'PUT',
             url: repo + fileName,
             validResponseCodes: '201',
