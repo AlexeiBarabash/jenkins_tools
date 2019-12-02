68d12a5577c9fc31b52f2ca74cc0900dbc67fbeb
Bar Nuri <barn@codeoasis.com>
2019-12-02 20:44:59 +0200

    repo

--- vars/publishZipArtifactToNexus.groovy
+++ vars/publishZipArtifactToNexus.groovy
@@ -1,13 +1,13 @@
 
 #!/usr/bin/env groovy
-def call(filePath, repo = ,user = 'jenkins:zxasqw12') {
+def call(filePath, repo = 'http://registry.codeoasis.com:8081/repository/Files/',user = 'jenkins:zxasqw12') {
     textWithColor("ENV")
     def fileName = JOB_NAME + '_' + BUILD_NUMBER + '.zip'
     def auth = 'Basic ' + user.bytes.encodeBase64().toString()
     httpRequest (consoleLogResponseBody: true,
             uploadFile: filePath
             httpMode: 'PUT',
-            url: "http://registry.codeoasis.com:8081/repository/Files/" + fileName,
+            url: repo + fileName,
             validResponseCodes: '201',
             customHeaders: [[maskValue: false, name: 'Authorization', value: auth]]
     )
