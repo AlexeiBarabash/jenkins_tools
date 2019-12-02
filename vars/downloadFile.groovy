84560f7eae673599fc91f5b5cee2ffd254c392a5
Bar Nuri <barn@codeoasis.com>
2019-12-02 21:11:07 +0200

    fix download

--- vars/downloadFile.groovy
+++ vars/downloadFile.groovy
@@ -2,7 +2,7 @@
 
 def call(outputFile ,urlPath, basicAuth = 'jenkins:zxasqw12') {
     textWithColor("Download File")
-    def auth = 'Basic ' + user.bytes.encodeBase64().toString()
+    def auth = 'Basic ' + basicAuth.bytes.encodeBase64().toString()
     httpRequest (consoleLogResponseBody: true,
             httpMode: 'GET',
             outputFile: outputFile,
