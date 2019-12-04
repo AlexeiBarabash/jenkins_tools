e4446f05652ea792e0777ab08f6e5a1c46985b05
Bar Nuri <barn@codeoasis.com>
2019-12-04 18:08:25 +0200

    fix clone

--- vars/gitClone.groovy
+++ vars/gitClone.groovy
@@ -9,10 +9,19 @@ def call() {
         submoduleCfg: [],
         userRemoteConfigs: [[url: GIT_REPO]]
     ])
-    bashCommand("git submodule update --init --recursive || true")
-    bashCommand("git submodule foreach 'git checkout ${BRANCH_TO_CLONE} || true'")
-    bashCommand('git reset --hard || true')
-    bashCommand('git clean -dfx || true')
-    bashCommand("ls -latr")   
+    if(isWindows()) {
+        powershell "git submodule update --init --recursive"
+        powershell "git submodule foreach 'git checkout ${BRANCH_TO_CLONE}'"
+        powershell 'git reset --hard'
+        powershell 'git clean -dfx'
+        powershell "dir" 
+    } else {
+        sh "git submodule update --init --recursive || true"
+        sh "git submodule foreach 'git checkout ${BRANCH_TO_CLONE} || true'"
+        sh 'git reset --hard || true'
+        sh 'git clean -dfx || true'
+        sh "ls -latr"
+    }
+ 
     textWithColor("Finish Git Clone - ${BRANCH_TO_CLONE}")
 }
