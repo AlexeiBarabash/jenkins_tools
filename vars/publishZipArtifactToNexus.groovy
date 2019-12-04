73cd2cbad77913d30239c0377a9866b985126219
Bar Nuri <barn@codeoasis.com>
2019-12-04 18:24:46 +0200

    fix publish

--- vars/publishZipArtifactToNexus.groovy
+++ vars/publishZipArtifactToNexus.groovy
@@ -2,7 +2,7 @@
 
 def call(filePath, repo = 'http://registry.codeoasis.com:8081/repository/Files/', user = 'jenkins:zxasqw12') {
     textWithColor("Publish Zip Artifact To Nexus - ${filePath}")
-    def fileName = JOB_NAME + '_' + BUILD_NUMBER + '.zip'
+    def fileName = (JOB_NAME + '_' + BUILD_NUMBER + '.zip').replace(" ","_")
     def auth = 'Basic ' + user.bytes.encodeBase64().toString()
     bashCommand("curl -v --user '${user}' --upload-file ${filePath} ${repo + fileName}")
     textWithColor("Done Publish Zip Artifact To Nexus")
