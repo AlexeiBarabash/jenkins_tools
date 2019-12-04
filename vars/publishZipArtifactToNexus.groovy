180683948120138666735e3d9c5a25fc178cab34
Bar Nuri <barn@codeoasis.com>
2019-12-04 12:06:54 +0200

    fileupload

--- vars/publishZipArtifactToNexus.groovy
+++ vars/publishZipArtifactToNexus.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def call(filePath, repo = 'http://registry.codeoasis.com:8081/repository/Files/', user = 'jenkins:zxasqw12') {
-    textWithColor("Publish Zip Artifact To Nexus")
+    textWithColor("Publish Zip Artifact To Nexus - ${filePath}")
     def fileName = JOB_NAME + '_' + BUILD_NUMBER + '.zip'
     def auth = 'Basic ' + user.bytes.encodeBase64().toString()
     httpRequest (consoleLogResponseBody: true,
