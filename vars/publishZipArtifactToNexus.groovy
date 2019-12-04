c95c44efda00cc2d2bb993c598d5f19085dd69a6
Bar Nuri <barn@codeoasis.com>
2019-12-04 12:41:21 +0200

    publish

--- vars/publishZipArtifactToNexus.groovy
+++ vars/publishZipArtifactToNexus.groovy
@@ -4,14 +4,15 @@ def call(filePath, repo = 'http://registry.codeoasis.com:8081/repository/Files/'
     textWithColor("Publish Zip Artifact To Nexus - ${filePath}")
     def fileName = JOB_NAME + '_' + BUILD_NUMBER + '.zip'
     def auth = 'Basic ' + user.bytes.encodeBase64().toString()
-    httpRequest (consoleLogResponseBody: true,
-            contentType: 'multipart/form-data',
-            uploadFile: filePath,
-            httpMode: 'PUT',
-            url: repo + fileName,
-            validResponseCodes: '201',
-            customHeaders: [[maskValue: false, name: 'Authorization', value: auth]]
-    )
+    // httpRequest (consoleLogResponseBody: true,
+    //         contentType: 'multipart/form-data',
+    //         uploadFile: filePath,
+    //         httpMode: 'PUT',
+    //         url: repo + fileName,
+    //         validResponseCodes: '201',
+    //         customHeaders: [[maskValue: false, name: 'Authorization', value: auth]]
+    // )
+    powershell "bash -c \"curl -v --user '${user}' --upload-file ${filePath} ${repo + fileName}\""
     textWithColor("Done Publish Zip Artifact To Nexus")
 
     return repo + fileName
