2d9d7dbd930e36ca34f1ccdce285d7025d171c1b
Bar Nuri <barn@codeoasis.com>
2019-12-04 12:58:20 +0200

    download

--- vars/downloadFile.groovy
+++ vars/downloadFile.groovy
@@ -1,14 +1,21 @@
 #!/usr/bin/env groovy
 
-def call(outputFile ,urlPath, basicAuth = 'jenkins:zxasqw12') {
+def call(outputFile ,urlPath,isWindows = false, basicAuth = 'jenkins:zxasqw12') {
     textWithColor("Download File")
-    def auth = 'Basic ' + basicAuth.bytes.encodeBase64().toString()
-    httpRequest (consoleLogResponseBody: true,
-            httpMode: 'GET',
-            outputFile: outputFile,
-            url: urlPath,
-            responseHandle: 'NONE',
-            customHeaders: [[maskValue: false, name: 'Authorization', value: auth]]
-    )
+    // def auth = 'Basic ' + basicAuth.bytes.encodeBase64().toString()
+    // httpRequest (consoleLogResponseBody: true,
+    //         httpMode: 'GET',
+    //         outputFile: outputFile,
+    //         url: urlPath,
+    //         responseHandle: 'NONE',
+    //         customHeaders: [[maskValue: false, name: 'Authorization', value: auth]]
+    // )
+    def curlCmd = "curl -v --user '${basicAuth}' --output ${outputFile} ${urlPath}"
+    if(isWindows) {
+        powershell "C:\\\"Program Files\"\\Git\\bin\\bash.exe -c \"${curlCmd}\""
+    } else {
+        sh curlCmd
+    }
+
     textWithColor("Done Download File")
 }
