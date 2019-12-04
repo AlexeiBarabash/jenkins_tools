fafc019b9a41f25dfac9c6bd7bb680d755325a15
Bar Nuri <barn@codeoasis.com>
2019-12-04 12:51:49 +0200

    publish

--- vars/publishZipArtifactToNexus.groovy
+++ vars/publishZipArtifactToNexus.groovy
@@ -1,18 +1,15 @@
 #!/usr/bin/env groovy
 
-def call(filePath, repo = 'http://registry.codeoasis.com:8081/repository/Files/', user = 'jenkins:zxasqw12') {
+def call(filePath,isWindows = false, repo = 'http://registry.codeoasis.com:8081/repository/Files/', user = 'jenkins:zxasqw12') {
     textWithColor("Publish Zip Artifact To Nexus - ${filePath}")
     def fileName = JOB_NAME + '_' + BUILD_NUMBER + '.zip'
     def auth = 'Basic ' + user.bytes.encodeBase64().toString()
-    // httpRequest (consoleLogResponseBody: true,
-    //         contentType: 'multipart/form-data',
-    //         uploadFile: filePath,
-    //         httpMode: 'PUT',
-    //         url: repo + fileName,
-    //         validResponseCodes: '201',
-    //         customHeaders: [[maskValue: false, name: 'Authorization', value: auth]]
-    // )
-    powershell "C:\\\"Program Files\"\\Git\\bin\\bash.exe -c \"curl -v --user '${user}' --upload-file ${filePath} ${repo + fileName}\""
+    def curlCmd = "curl -v --user '${user}' --upload-file ${filePath} ${repo + fileName}"
+    if(isWindows) {
+        powershell "C:\\\"Program Files\"\\Git\\bin\\bash.exe -c \"${curlCmd}\""
+    } else {
+        sh curlCmd
+    }
     textWithColor("Done Publish Zip Artifact To Nexus")
 
     return repo + fileName
