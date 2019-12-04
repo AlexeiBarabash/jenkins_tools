5ad2e9ef27e285a97f82939a038ef816ddc13891
Bar Nuri <barn@codeoasis.com>
2019-12-04 20:31:39 +0200

    needTrigger

--- vars/needToTrigger.groovy
+++ vars/needToTrigger.groovy
@@ -1,21 +1,20 @@
 #!/usr/bin/env groovy
 def call(val) {
     def changeLogSets = currentBuild.changeSets
-    def arr = new ArrayList()
-    print(changeLogSets)
     for (int i = 0; i < changeLogSets.size(); i++) {
         def entries = changeLogSets[i].items
         for (int j = 0; j < entries.length; j++) {
             def entry = entries[j]
-            echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
             def files = new ArrayList(entry.affectedFiles)
-            print(files)
-            arr = arr + files
             for (int k = 0; k < files.size(); k++) {
                 def file = files[k]
-                echo "  ${file.editType.name} ${file.path}"
-                echo file.path.substring(file.path.lastIndexOf('.')+1)
+                def ext = (file.path.substring(file.path.lastIndexOf('.')+1)).toLowerCase()
+                if(ext != 'groovy' && ext != 'md') {
+                    echo "  ${file.editType.name} ${file.path}"
+                    return true
+                }
             }
         }
     }
+    return false
 }
