8e3e9c1c4af8f31b8a7ea3f2ebcdfc37d72b150f
Bar Nuri <barn@codeoasis.com>
2019-12-05 11:51:39 +0200

    jira

--- vars/jiraWebhooks.groovy
+++ vars/jiraWebhooks.groovy
@@ -22,6 +22,7 @@ def call(integrationIsQa = false) {
         //     messageForWebhook = messageForWebhook + message + "\n"
         //     // println(commit.getChanges())
         // }
+        def changeLogSets = currentBuild.changeSets
         for (int i = 0; i < changeLogSets.size(); i++) {
             def entries = changeLogSets[i].items
             for (int j = 0; j < entries.length; j++) {
