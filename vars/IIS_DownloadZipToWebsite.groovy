538f8ff588281c58cfd59b97bc7e8bdc12144968
Bar Nuri <barn@codeoasis.com>
2019-12-05 07:46:18 +0200

    IIS_DownloadZipToWebsite

--- vars/IIS_DownloadZipToWebsite.groovy
+++ vars/IIS_DownloadZipToWebsite.groovy
@@ -1,7 +1,9 @@
 #!/usr/bin/env groovy
 def call() {
+    textWithColor("IIS_DownloadZipToWebsite")
     bashCommand("rm -rf ./Artifact.zip")
     downloadFile('./Artifact.zip',env.ArtifactUrl)
     bashCommand("ls -latr")
     unzip dir: env.IIS_FOLDER, zipFile: './Artifact.zip'
+    textWithColor("DONE IIS_DownloadZipToWebsite")
 }
