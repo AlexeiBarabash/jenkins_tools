014dcab7e6c9a78a299a23705bc12e1b85b59ccd
Bar Nuri <barn@codeoasis.com>
2019-12-05 09:16:45 +0200

    getBuildNameFromCommits

---
+++ vars/getBuildNameFromCommits.groovy
@@ -0,0 +1,26 @@
+#!/usr/bin/env groovy
+def call() {
+    try {
+        textWithColor("getBuildNameFromCommits")
+        def changeLogSets = currentBuild.changeSets
+        def maxDate = null
+        def builderName = "unknown"
+        for (int i = 0; i < changeLogSets.size(); i++) {
+            def entries = changeLogSets[i].items
+            for (int j = 0; j < entries.length; j++) {
+                def entry = entries[j]
+                def entryDate = new Date(entry.timestamp)
+                def needToUpdateMaxDate = maxDate == null || maxDate < entryDate
+                maxDate = needToUpdateMaxDate ? entryDate : maxDate
+                builderName = entry.author
+            }
+        }
+        env.BUILDER_NAME = builderName
+    } catch(Exception ex) {
+        env.BUILDER_NAME = 'unknown'
+        textWithColor("getBuildNameFromCommits Error")
+        echo ex.toString()
+    }
+}
+
+
--- vars/init.groovy
+++ vars/init.groovy
@@ -2,18 +2,13 @@
 def call(autoDetectEnv = false,autoDetectEnvFirstLetterUpper = false) {
     textWithColor("Init")
 
-
     def changeLogSets = currentBuild.changeSets
+    def maxDate 
     for (int i = 0; i < changeLogSets.size(); i++) {
         def entries = changeLogSets[i].items
         for (int j = 0; j < entries.length; j++) {
             def entry = entries[j]
             echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
-            def files = new ArrayList(entry.affectedFiles)
-            for (int k = 0; k < files.size(); k++) {
-                def file = files[k]
-                echo "  ${file.editType.name} ${file.path}"
-            }
         }
     }
 
@@ -85,6 +80,7 @@ def call(autoDetectEnv = false,autoDetectEnvFirstLetterUpper = false) {
         env.BUILDER_NAME = 'unknown'
         textWithColor("BuildUser Error")
         echo ex.toString()
+        getBuildNameFromCommits()
     }
 
     
