deb488183e3cde732bfb78ba957f6548e6f9b80f
Bar Nuri <barn@codeoasis.com>
2019-12-09 14:52:15 +0200

    nexus repo

--- vars/publishZipArtifactToNexus.groovy
+++ vars/publishZipArtifactToNexus.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(filePath, repo = 'http://registry.codeoasis.com:8081/repository/Files/', user = 'jenkins:zxasqw12') {
+def call(filePath, repo = 'https://nexus.codeoasis.com/repository/Files/', user = 'jenkins:zxasqw12') {
     textWithColor("Publish Zip Artifact To Nexus - ${filePath}")
     def fileName = (JOB_NAME + '_' + BUILD_NUMBER + '.zip').replace(" ","_")
     def auth = 'Basic ' + user.bytes.encodeBase64().toString()
