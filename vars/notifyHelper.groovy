bc9f7e5ba042f9b72bb23106487978e1543024dd
Bar Nuri <barn@codeoasis.com>
2019-12-15 13:51:36 +0200

    notifyHelper default value

--- vars/notifyHelper.groovy
+++ vars/notifyHelper.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
-def call(checkTriggered = true, ignoreResult = false) {
+def call(checkTriggered = false, ignoreResult = false) {
     teamsSendHelper(checkTriggered, ignoreResult)
     newSlackSendHelper(checkTriggered, ignoreResult)
 }
