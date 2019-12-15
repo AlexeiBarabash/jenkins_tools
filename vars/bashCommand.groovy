629bbe1812761debf265842bdc61d0f4fd638781
Bar Nuri <barn@codeoasis.com>
2019-12-15 13:36:18 +0200

    bashcommand

--- vars/bashCommand.groovy
+++ vars/bashCommand.groovy
@@ -1,8 +1,11 @@
 #!/usr/bin/env groovy
 def call(cmd) {
+    def res = null
     if(isWindows()) {
-        powershell (returnStdout: true, script:"C:\\\"Program Files\"\\Git\\bin\\bash.exe -c \"${cmd}\"")
-        return
+        res = powershell (returnStdout: true, script:"C:\\\"Program Files\"\\Git\\bin\\bash.exe -c \"${cmd}\"")
+    } else {
+        res = sh(returnStdout: true, script:cmd)
     }
-    sh(returnStdout: true, script:cmd)
+    echo res
+    return res
 }
