ee9c7ba581d575e91ca83678012d40012057e1bd
Bar Nuri <barn@codeoasis.com>
2020-01-22 19:27:50 +0200

    Update gitClone.groovy

--- vars/gitClone.groovy
+++ vars/gitClone.groovy
@@ -11,14 +11,15 @@ def call(cleanCache = true, credentialsId = null) {
         userRemoteConfigs: [[url: GIT_REPO, credentialsId: credentialsId]]
     ])
     textWithColor("Git Clone - Cleans")
-    bashCommand("git submodule update --init --recursive || true")
-    bashCommand("git submodule foreach 'git fetch --all || true'")
-    bashCommand("git submodule foreach 'git checkout ${BRANCH_TO_CLONE} -f || true'")
-    bashCommand("git submodule foreach 'git pull -f || true'")
     bashCommand('git reset --hard || true')
     if(cleanCache) {
         bashCommand('git clean -dfx || true')
     }
+    textWithColor("Git Clone - Fetch & Pull")
+    bashCommand("git submodule update --init --recursive || true")
+    bashCommand("git submodule foreach 'git fetch --all || true'")
+    bashCommand("git submodule foreach 'git checkout ${BRANCH_TO_CLONE} -f || true'")
+    bashCommand("git submodule foreach 'git pull -f || true'")
     bashCommand("ls -latr")
     env.LastCommitMessage = bashCommand("git log -1 --format=%s").replace("\n", "")
     env.LastCommitHash = bashCommand("git log -1 --format=%h").replace("\n", "")
