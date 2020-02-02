d9e09de54a5ae3026c6406a83b112692cae38e9c
Bar Nuri <barn@codeoasis.com>
2020-02-02 13:00:33 +0200

    output

--- vars/downloadFile.groovy
+++ vars/downloadFile.groovy
@@ -5,7 +5,7 @@ def call(outputFile ,urlPath, basicAuth = 'jenkins:zxasqw12') {
     echo urlPath
     def userAndPass = basicAuth.split(':')
     // bashCommand("curl --insecure -v --user '${basicAuth}' --output ${outputFile} --retry 999 --retry-max-time 0 ${urlPath}")
-    bashCommand("wget --user ${userAndPass[0]} --password ${userAndPass[1]} --no-check-certificate --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 ${urlPath}")
+    bashCommand("wget -O ${outputFile} --user ${userAndPass[0]} --password ${userAndPass[1]} --no-check-certificate --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 ${urlPath}")
     textWithColor("Done Download File", "green")
 }
 
