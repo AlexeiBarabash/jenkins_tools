744c8f9929d37b5b256d4c4ff44c6417228c1d6b
Bar Nuri <barn@codeoasis.com>
2020-02-02 13:04:13 +0200

    bashcmd

--- vars/bashCommand.groovy
+++ vars/bashCommand.groovy
@@ -10,6 +10,8 @@ def call(cmd) {
     try {
         def res = null
         if(isWindows) {
+            cmd = cmd.replace('"','\"')
+            cmd = cmd.replace('\\','\\\\')
             res = powershell (returnStdout: true, script:"C:\\\"Program Files\"\\Git\\bin\\bash.exe -c \"${cmd}\"")
         } else {
             res = sh(returnStdout: true, script:cmd)
