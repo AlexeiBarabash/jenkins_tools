8e565cf97bd4a745f90fdd165e0acd7b28d695bd
Bar Nuri <barn@codeoasis.com>
2020-02-02 12:06:04 +0200

    env

--- vars/isWindows.groovy
+++ vars/isWindows.groovy
@@ -1,6 +1,7 @@
 #!/usr/bin/env groovy
 def call() {
     try {
+        echo "env.isWindows = ${env.isWindows}"
         return env.isWindows == "true" || NODE_LABELS.toLowerCase().indexOf('windows') >= 0 || NODE_NAME.toLowerCase().indexOf('windows') >= 0
     } catch(Exception ex) {
         return false
