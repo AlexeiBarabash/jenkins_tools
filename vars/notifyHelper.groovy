427f092d9c18a164b4e76230fd13470ef6b2f0fb
Bar Nuri <barnuri@hotmail.com>
2020-02-11 16:45:05 +0200

    notifyHelper

--- vars/notifyHelper.groovy
+++ vars/notifyHelper.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
-def call(checkTriggered = false, ignoreResult = false) {
+def call(checkTriggered = false, ignoreResult = true) {
     teamsSendHelper(checkTriggered, ignoreResult)
     newSlackSendHelper(checkTriggered, ignoreResult)
 }
